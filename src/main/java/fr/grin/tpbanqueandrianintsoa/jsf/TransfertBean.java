b5cf2810c3f34d6b53b4661ff12839d3dc304c55
Andrianintsoa <andrianintsoat@gmail.com>
2023-04-13 23:03:03 +0300

    Traitement d' erreur et messade de succès

--- src/main/java/fr/grin/tpbanqueandrianintsoa/jsf/TransfertBean.java
+++ src/main/java/fr/grin/tpbanqueandrianintsoa/jsf/TransfertBean.java
@@ -6,7 +6,10 @@ package fr.grin.tpbanqueandrianintsoa.jsf;
 
 import fr.grin.tpbanqueandrianintsoa.ejb.GestionnaireCompte;
 import fr.grin.tpbanqueandrianintsoa.entities.CompteBancaire;
+import fr.grin.tpbanqueandrianintsoa.jsf.util.Util;
 import jakarta.ejb.EJB;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.application.FacesMessage.Severity;
 import jakarta.inject.Named;
 import jakarta.faces.view.ViewScoped;
 import java.io.Serializable;
@@ -51,12 +54,51 @@ public class TransfertBean implements Serializable {
     
     
      public String submitTransfert(){
+        if(idSource == idDestination){
+            Util.messageErreur("Veuillez choisir un autre compte", "", "form:source");
+           return null;
+        }
         CompteBancaire compteSource = gestionCompte.findById(idSource);
-        CompteBancaire compteDestination = gestionCompte.findById(idDestination);
-        gestionCompte.transferer(compteSource, compteDestination, montant);
-        return "listeComptes?faces-redirect=true";
+        CompteBancaire compteDestination = gestionCompte.findById(idDestination); 
+        Severity severity = FacesMessage.SEVERITY_ERROR;
+        Boolean test = true;
+        if(compteSource == null){
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+           test = false;
+        }
+        if(compteDestination == null){
+           Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+           test = false;
+        }
+        if(montant <= 0){
+           Util.messageErreur("Le montant doit être supérieur à 0", "Le montant doit être supérieur à 0", "form:montant");
+           test = false;
+        }
         
+        if(test){
+            
+            Severity severityUpdate = null;  
+            String summaryUpdate = "";
+            String messageUpdate = "";
+            try{
+                gestionCompte.transferer(compteSource, compteDestination, montant);
+                severity = FacesMessage.SEVERITY_INFO;
+                summaryUpdate = "Le transfert de " + compteSource.getNom() + " de " + montant + "€ a été enregistré";
+            }catch(Exception e){
+                severity = FacesMessage.SEVERITY_ERROR;
+                summaryUpdate = "transfer failed";
+                messageUpdate = e.getMessage();
+
+            }finally{
+                FacesMessage facesMessage = new FacesMessage(severity, summaryUpdate, messageUpdate);
+                //FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+                Util.addFlashMessage(facesMessage);
+            }
+            
+            return "listeComptes?faces-redirect=true";
+        }
         
+        return "";
     }
     /**
      * Creates a new instance of TransfertBean
